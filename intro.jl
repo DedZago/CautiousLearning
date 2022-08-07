618aebef07738d222f9af8645ee364ddd90bde73
Daniele Zago <zagodaniele.9@gmail.com>
2022-08-07 15:17:11 +0200

    saControlLimits and a working example

--- intro.jl
+++ intro.jl
@@ -7,70 +7,41 @@ using Distributions
 
 include(srcdir("generate_data.jl"))
 include(srcdir("update_parameter.jl"))
+include(srcdir("simulate_runs.jl"))
 
-function chart_statistic(x, thetaHat)
-    return (x - thetaHat)/sqrt(thetaHat)
-end
 
-function runSimulation(ch, um, thetaHat, D, m; IC=true, tau=1, delta=0.0, maxrl=1e04)
-    thetaHatVec = zeros(maxrl)
-    thetaHatVec[1] = thetaHat
-    di = 1
-    diVec = Array{Int}(undef, maxrl)
-    diVec[1] = di
-    thetaHatCaut = thetaHat
-    thetaHatCautVec = zeros(maxrl)
-    thetaHatCautVec[1] = thetaHatCaut
-
-    valueVec = zeros(maxrl)
-    valueVec[1] = get_value(ch)
-    i = 1
-    while i < maxrl
-        thetaHatCaut = thetaHatVec[i - di + 1]
-        y = gen_data_seq(D, i, IC=IC, tau=tau, delta=delta)
-        ch = update_series(ch, chart_statistic(y, thetaHatCaut))
-        thetaHat = update_parameter(thetaHat, y, i + m)
-        i += 1
-        valueVec[i] = get_value(ch)
-        thetaHatVec[i] = thetaHat
-        if check_update(ch, um)
-            di = 1
-        else
-            di += 1
-        end
-        diVec[i] = di
-        thetaHatCautVec[i] = thetaHatCaut
-        if check_OC(ch)
-            return (t_alarm = i, chart_values = valueVec[1:i], limit_alarm = get_limits(ch), limit_cautious = get_warning_limit(um), parameter_updates = thetaHatCautVec[1:i], di = diVec[1:i])
-        end
-
-    end
-
-    return (t_alarm = maxrl, chart_values = valueVec, limit_alarm = get_limits(ch), limit_cautious = get_warning_limit(um), parameter_updates = thetaHatCautVec, di = diVec)
-end
-
-function runExperiment(ch, um, D, m; IC=true, tau=1, delta=0.0, maxrl=1e04)
-    yinit = rand(D, m)
-    thetaHat = mean(yinit)
-
-    return runSimulation(ch, um, thetaHat, D, m, IC=IC, tau=tau, delta=delta, maxrl=maxrl)
-end
-
-ch = AEWMA(l=0.15, L=1.2)
-um = CautiousLearning(L=0.2)
-# um = SelfStarting()
+ch = AEWMA(l=0.15, L=1.5)
+# um = CautiousLearning(L=0.2)
+um = SelfStarting()
 # um = FixedParameter()
-D = Poisson(5.0)
+theta = 5.0
+D = Poisson(theta)
 m = 50
 maxrl = 3000
-IC = false
-delta = 0.5
-tau = 100
 
-tmp = runExperiment(ch, um, D, m, maxrl=maxrl, IC=IC, delta=delta, tau=tau)
+Arl0 = 8
+
+sa_cl = saControlLimits(ch, um, runSimulation, Arl0, theta, D, m, verbose=false, Amin=0.1, maxiter=1e05, z=1.7)
+
+ch = typeof(ch)(ch, L = sa_cl[:h])
+mean([runSimulation(ch, um, theta, D, m, maxrl=maxrl)[:t_alarm] for _ in 1:10000])
+
+ch = AEWMA(l=0.15, L=1.2)
+um = CautiousLearning(L = sa_cl[:h])
+# um = SelfStarting()
+# um = FixedParameter()
+
+IC = false
+delta = 0.75
+tau = 50
+tmpList = [runExperiment(ch, um, D, m, IC=IC, delta=delta, tau=tau) for _ in 1:10000]
+
+mean([sim[:t_alarm] for sim in tmpList])
+
+
+tmp = runExperiment(ch, um, D, m, IC=IC, delta=delta, tau=tau)
 
 using Plots
-
 plot(tmp[:chart_values], legend=:outerright, label="C")
 hline!([-tmp[:limit_alarm],tmp[:limit_alarm]], fill=true, fillcolour=:orange, fillalpha=0.15, color=:darkred, style=:dash, label=false)
 hline!([-tmp[:limit_cautious],tmp[:limit_cautious]], fill=true, fillcolour=:lightgreen, fillalpha=0.35, color=:darkgreen, style=:dash, label=false)
@@ -87,3 +58,25 @@ plot(tmp[:di])
 if !IC
     vline!([tau], style=:dash, colour=:darkblue, label="tau")
 end
+
+
+#! Implement GICP using confidence intervals
+# outLow = stochasticApproximationPCC(alpha, runSim, ARL0, model, θlow, direction, Amin=0.001, maxiter=4e04, z = 1.5)
+# outUp = stochasticApproximationPCC(alpha, runSim, ARL0, model, θup, direction, Amin=0.001, maxiter=4e04, z = 1.5)
+
+# limits = (outLow[1], outUp[1])
+# L = minimum(limits)
+
+# tmp = [runSim(L, model, θ, maxrl = 1e04, direction=direction) for _ in 1:1e04]
+# mean(tmp)
+
+# B = 100
+# Lvec = zeros(B)
+# for b in 1:B
+#     println("b: ", b)
+#     θsim = rand(get_posterior(model))
+#     out = stochasticApproximationPCC(alpha, runSim, ARL0, model, θsim, direction, Amin=0.001, maxiter=4e04, z = 1.5, verbose=false)
+#     Lvec[b] = out[1]
+# end
+# L = quantile(Lvec, beta)
+# density(Lvec)
---
+++ src/simulate_runs.jl
@@ -0,0 +1,99 @@
+
+function chart_statistic(x, thetaHat)
+    return (x - thetaHat)/sqrt(thetaHat)
+end
+
+function runSimulation(ch, um, thetaHat, D, m; IC=true, tau=1, delta=0.0, maxrl=Int(1e04))
+    maxrl_i = Int(round(maxrl))
+    thetaHatVec = zeros(maxrl_i)
+    thetaHatVec[1] = thetaHat
+    di = 1
+    diVec = Array{Int}(undef, maxrl_i)
+    diVec[1] = di
+    thetaHatCaut = thetaHat
+    thetaHatCautVec = zeros(maxrl_i)
+    thetaHatCautVec[1] = thetaHatCaut
+
+    valueVec = zeros(maxrl_i)
+    valueVec[1] = get_value(ch)
+    i = 1
+    while i < maxrl_i
+        thetaHatCaut = thetaHatVec[i - di + 1]
+        y = gen_data_seq(D, i, IC=IC, tau=tau, delta=delta)
+        ch = update_series(ch, chart_statistic(y, thetaHatCaut))
+        thetaHat = update_parameter(thetaHat, y, i + m)
+        i += 1
+        valueVec[i] = get_value(ch)
+        thetaHatVec[i] = thetaHat
+        if check_update(ch, um)
+            di = 1
+        else
+            di += 1
+        end
+        diVec[i] = di
+        thetaHatCautVec[i] = thetaHatCaut
+        if check_OC(ch)
+            break
+        end
+
+    end
+
+    return @NamedTuple{t_alarm::Int64, chart_values::Vector{Float64}, limit_alarm::Float64, limit_cautious::Float64, parameter_updates::Vector{Float64}, di::Vector{Int64}}((t_alarm = i, chart_values = valueVec[1:i], limit_alarm = get_limits(ch), limit_cautious = get_warning_limit(um), parameter_updates = thetaHatCautVec[1:i], di = diVec[1:i]))
+
+end
+
+function runExperiment(ch, um, D, m; IC=true, tau=1, delta=0.0, maxrl=1e04)
+    yinit = rand(D, m)
+    thetaHat = mean(yinit)
+
+    return runSimulation(ch, um, thetaHat, D, m, IC=IC, tau=tau, delta=delta, maxrl=maxrl)
+end
+
+
+#! TODO: adapt to work with control charts
+"""
+	stochasticApproximationPCC(hstart::Real, rlsim::Function, Arl0::Real; kw...)
+
+Computes the control limit for a control chart such that it satisfies E[RL] = Arl0.
+"""
+#? Set hstart to CH
+#? 
+function saControlLimits(ch, um, rlsim::Function, Arl0, thetaHat, Dist, m; Nfixed=500, Afixed=0.1, Amin=0.1, Amax=100, delta=0.1, q=0.55, gamma=0.02, Nmin=1000, z = 3.0, Cmrl=10.0, maxiter = 4e05, verbose=true, eps = 1e-04)
+    v = (z/gamma)^2
+    sm = 0.0
+    sp = 0.0
+    u = 0.0
+
+    h = get_limits(ch)
+    rm = rp = hm = score = s2 = D = 0.0
+
+    # Skip adaptation
+    D = Amin
+    i = 0
+
+
+    if verbose println("Running SA ...") end
+
+    while i < maxiter
+        if verbose && i % floor(maxiter / 20) == 0
+            println("i: ", i, "/", Integer(maxiter))
+        end
+        i += 1
+        ch_temp = typeof(ch)(ch, L = h)                # Copy chart ch with hstart as control limit
+        rl = rlsim(ch_temp, um, thetaHat, Dist, m, IC=true, maxrl = Int(round(Cmrl * Arl0 * sqrt(i + Nfixed))))[:t_alarm]
+        score = (rl - Arl0)/Arl0
+
+        h = max(eps, h - D * score / (i^q))
+        hm = hm + (h - hm) / float(i)
+        s2 = s2 + (score * score - s2) / float(i)
+
+        if (i > Nmin) && (i > v * s2)
+            if verbose println("Convergence!") end
+            break
+        end
+    end
+    
+    if verbose println("Done.") end
+
+    return (h=hm, iter=i)
+end
