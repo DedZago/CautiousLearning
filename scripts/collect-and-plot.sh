4e10c316ac79d26488c40dfcc11bdca254467999
Daniele Zago <zagodaniele.9@gmail.com>
2022-08-21 17:52:51 +0200

    Update collect-and-plot.sh

--- scripts/collect-and-plot.sh
+++ scripts/collect-and-plot.sh
@@ -1,6 +1,5 @@
 julia summary/collect.jl
 Rscript summary/plots-tables.R
-../data/sims
 
 # find ../data/sims/ -name '*.png' | cpio -pdm  ../../CautiousBootstrap/papers/main-paper/figures/cautious-learning-fixed/
 cp -r ../plots/sims ../../CautiousBootstrap/papers/main-paper/figures/
