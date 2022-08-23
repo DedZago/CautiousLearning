46a5b190c4bc8c1123ed4ea7cddde11b9454f37d
Daniele Zago <zagodaniele.9@gmail.com>
2022-08-23 09:40:15 +0200

    Update cfg.jl

--- src/cfg.jl
+++ src/cfg.jl
@@ -19,7 +19,7 @@ include(srcdir("update_parameter.jl"))
     ch = signedEWMA(l=0.2, L=0.8)
     um = CautiousLearning(ATS = 3)
     IC = [true, false]
-    delta = [0.35, 0.5, 0.75, 1.0, 1.25, 1.5]
+    delta = [0.25, 0.35, 0.5, 0.75, 1.0, 1.25, 1.5]
     tau = [1, 50, 100]
     beta = 0.1
     Arl0 = 500
@@ -41,7 +41,6 @@ Base.string(ch::UnivariateSeries) = string(typeof(ch)) * string(get_params(ch))
 
 # umVec = [CautiousLearning(ATS=0)]
 umVec = [FixedParameter(), AdaptiveEstimator(), CautiousLearning(ATS=0)]
-theta = 4.0
-D = Poisson(theta)
-config = [[SimulationSettings(ch = signedEWMA(l=0.023, L=1.0), D=Poisson(4.0), um = um, seed = 2022-08-17) for um in umVec]; [SimulationSettings(ch = signedAEWMA(l=0.023, k=7.74, L=0.5), D=Poisson(4.0), um = um, seed = 2022-08-17) for um in umVec]; [SimulationSettings(ch = signedEWMA(l=0.019, L=1.0), D=Poisson(7.0), um = um, seed = 2022-08-18) for um in umVec]; [SimulationSettings(ch = signedAEWMA(l=0.019, k=9.7699, L=0.5), D=Poisson(7.0), um = um, seed = 2022-08-18) for um in umVec]]
+seed = 2022-08-19
+config = [SimulationSettings(ch = signedAEWMA(l=0.033, k=4.16, L=0.2), D=Poisson(1.0), um = um, seed = seed) for um in umVec]
 # config = [SimulationSettings(ch = signedAEWMA(l=0.23, k=7.74), D=D, um = um, seed = 2022-08-16) for um in umVec]
