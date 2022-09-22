0e94e195dff2e1cf2d532288005213c0580d3d1a
Daniele Zago - ST <zagodaniele.9@gmail.com>
2022-09-22 12:37:29 +0200

    run-arl and run-gicp left simulations

--- scripts/simulate/run-ARL.jl
+++ scripts/simulate/run-ARL.jl
@@ -8,7 +8,14 @@ println("Starting simulations...")
 for cfg in config
     folder = "theta="*string(params(cfg.D)[1])*"_"*string(cfg.ch)
     println("Starting ", cfg.um)
-    @showprogress for i in 1:nsim_each
+    start_index = 1
+    if add_simulations
+        # If add_simulations, add them to already-present ones
+        current_folder = datadir("sims", folder, string(cfg.um), "arls")
+        start_index = length(readdir(current_folder)) + 1
+    end
+    @showprogress for i in start_index:(start_index + nsim_each)
+        @show i
         sleep(0.5)
         cfg_thread = SimulationSettings(cfg, simulation = i + (current_thread-1) * nsim_each)
         loadname = datadir("sims", folder, string(cfg_thread.um), "limits", savename(cfg_thread, "jld2"))
