be992a7d81adcbbc299f7fad57c56d8e62743a9f
Daniele Zago - ST <zagodaniele.9@gmail.com>
2022-09-22 12:37:00 +0200

    parsing add-simulations

--- scripts/simulate/load_cfg.jl
+++ scripts/simulate/load_cfg.jl
@@ -14,6 +14,11 @@ function parse_commandline()
             arg_type = Int
             default = 1
             required = true
+        "--add_simulations", "-a"
+            help = "Add simulations to the already-present simulated values instead of starting from zero."
+            arg_type = Bool
+            default = false
+            required = false
     end
     return parse_args(s)
 end
@@ -21,7 +26,7 @@ end
 parsed_args = parse_commandline()
 nsim_each = parsed_args["nsimulations_each"]
 current_thread = parsed_args["current_thread"]
-
+add_simulations = parsed_args["add_simulations"]
 
 println("Sourcing configuration...")
 include(srcdir("cfg.jl"))
